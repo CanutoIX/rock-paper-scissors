8805291c252b21779039d66ff395ca67e3144b8f
CanutoIX <caanutto@gmail.com>
2022-12-13 10:36:08 -0300

    Basic game of rock paper scissors

---
+++ app.js
@@ -0,0 +1,4 @@
+const computerChoice = document.getElementById('computer-choice')
+const userChoice = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
