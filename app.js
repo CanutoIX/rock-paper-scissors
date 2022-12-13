2a074fea1fa01b9842873e8b19b9e1409a18645a
CanutoIX <caanutto@gmail.com>
2022-12-13 11:00:26 -0300

    Final code. I'm going to stylish it later tho

--- app.js
+++ app.js
@@ -1,4 +1,53 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id 
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) // or possibleChoices.length
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'Its a draw!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper' ) {
+        result = 'You win!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'scissors' ) {
+        result = 'You lost!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors' ) {
+        result = 'You win!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock' ) {
+        result = 'You lost!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock' ) {
+        result = 'You win!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper' ) {
+        result = 'You lost!'
+    }
+    resultDisplay.innerHTML = result
+}
